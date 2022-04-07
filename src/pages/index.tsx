86070d4efb0377a0ad144edddef61dbf3b4bcb51
山下卓也 <yamashitatakuya@yamashitatakuyanoMacBook-Pro.local>
2022-04-07 18:02:45 +0900

    エラー修正済み

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,35 @@
+import Link from 'next/link';
 
-const Home = () => {
 
+const login = () => {
 
 return (  
-<div class="flex flex-col bg-gray-300">
-<div class="box-border h-full w-full p-4 border-1 border-gray-400 bg-gray-200">
-  <div class="h-30 w-30 bg-gray-200">
-   <div class="text-gray-700 text-center bg-gray-200 px-2 py-1 m-1">
+<div className="flex flex-col bg-gray-300">
+<div className="box-border h-full w-full p-4 border-1 border-gray-400 bg-gray-200">
+  <div className="h-30 w-30 bg-gray-200">
+   <div className="text-gray-700 text-center bg-gray-200 px-2 py-1 m-1">
     <h1>ログイン</h1>
    </div>
+  
+ 
 
   <form action="/send-data-here" method="post">
-
-
-
-     <div class="text-gray-700 text-center bg-gray-200 px-4 py-2 m-2">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="id"></label>
+     <div className="text-gray-700 text-center bg-gray-200 px-4 py-2 m-2">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="id"></label>
       <input type="text" id="first" name="first" placeholder="メールアドレス/id" />
      </div>
 
-
-
-    <div class="text-gray-700 text-center bg-gray-200 px-4 py-2 m-2">
-     <label for="last"></label>
-     <input type="password" id="last" name="last" placeholder="パスワード" />
+    <div className="text-gray-700 text-center bg-gray-200 px-4 py-2 m-2">
+     <label for="pw"></label>
+     <input type="password" id="last" name="pw" placeholder="パスワード" />
     </div>
 
-    <div class="text-gray-700 text-center bg-gray-200 px-4 py-2 m-2">
-     <button class="bg-red-500 hover:bg-red-700  text-white font-bold py-2 px-4 rounded-full">
-      ログイン
+    <div className="text-gray-700 text-center bg-gray-200 px-4 py-2 m-2">
+
+     <button className="bg-red-500 hover:bg-red-700  text-white font-bold py-2 px-4 rounded-full">
+      <Link href="/user">ログイン</Link>
      </button>
     </div>
-
   </form>
 
 
@@ -40,15 +38,9 @@ return (
   </div>
 </div>
 
-
-
-
-
-
-
  );
 };
 
-export default Home;
+export default login;
 
 
---
+++ src/pages/user.tsx
@@ -0,0 +1,28 @@
+// user
+const user = () => {
+
+
+return (  
+<div class="flex flex-col bg-gray-300">
+
+ 
+
+
+<h1>ユーザー画面</h1>
+
+</div>
+
+ 
+
+
+
+
+
+
+
+ );
+};
+
+export default user;
+
+
