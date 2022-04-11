503e850e7f2993167fb30ffac20383e88a1d018a
山下卓也 <yamashitatakuya@yamashitatakuyanoMacBook-Pro.local>
2022-04-11 17:37:43 +0900

    ユーザ一覧まで完了

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -5,59 +5,54 @@ console.log(useRouter().query)
 		<>
 		
 
- <div class="bg-gray-400">    
-　<header class="container mx-auto text-white">
-     <div class="flex justify-between items-center">
-      <h1 class="text-4xl font-semibold"></h1>
-      <div>{'UserName ' + useRouter().query.input}</div>
-     </div>
-　</header>
+ <div className="bg-gray-400">    
+	 <header className="container mx-auto text-white">
+		<div className="flex justify-between items-center">
+			<h1 className="text-4xl font-semibold"></h1>
+			<div>{'UserName ' + useRouter().query.input}</div>
+		</div>
+	 </header>
 </div>
 
-<div class="grid grid-rows-2 grid-flow-col ">
- <div class="row-span-3 border border-gray-600 ">
+<div className="grid grid-rows-2 grid-flow-col ">
+  <div className="row-span-3 border border-gray-600 ">
      <ul>
-       <li><a href="#" class="block px-8 py-2 hover:bg-gray-600">HOME</a></li>
-  　　　<li><a href="#" class="block px-8 py-2 hover:bg-gray-600">ユーザー管理</a></li>
+       <li><a href="user" className="block px-8 py-2 hover:bg-gray-600">HOME</a></li>
+       <li><a href="user_show" className="block px-8 py-2 hover:bg-gray-600">ユーザー管理</a></li>
      </ul>
-        </div>
-        <div class="row-span-2 col-span-2 border border-gray-600 ...">
-            <p>履歴</p>
-　　　　　　　　　<table class="table-15">
-            <thead　class="border-collapse border border-slate-400 ...">
-                <tr>
-                <th>日付</th>
+  </div>
+  <div className="row-span-2 col-span-2 border border-gray-600 ...">
+    <p>履歴</p>
+   <table className="table-15">
+     <thead className="border-collapse border border-slate-400 ...">
+        <tr>
+         <th>日付</th>
+         <th>ユーザー</th>
+         <th>内容</th>
+        </tr>
+     </thead>
+     <tbody className="border-collapse border border-slate-400 ...">
+        <tr>
+          <th>2022/3/4</th>
+          <th>user1</th>
+          <th>ユーザーを作成しました</th>
+        </tr>
+        <tr>
+          <th>2022/3/3</th>
+          <th>user2</th>
+          <th>ユーザーを作成しました</th>
+        </tr>
+        <tr>
+          <th>2022/3/2</th>
+          <th>user3</th>
+          <th>ユーザーを作成しました</th>
+        </tr>
+     </tbody>
+   </table>
+  </div>
+  </div>
 
-                <th>ユーザー</th>
-                <th>内容</th>
-            </tr>
-            </thead>
-
-            <tbody　class="border-collapse border border-slate-400 ...">
-            <tr>
-            <th>2022/3/４</th>
-            <th>user１</th>
-            <th>ユーザーを作成しました</th>
-            </tr>
-            <tr>
-            <th>2022/3/３</th>
-            <th>user２</th>
-            <th>ユーザーを作成しました</th>
-            </tr>
-            <tr>
-            <th>2022/3/2</th>
-            <th>user３</th>
-            <th>ユーザーを作成しました</th>
-            </tr>
-
-            </tbody>
-        </table>
-            </div>
-        </div>
-
-
-
-		</>
+ </>
 
 
 
--- src/pages/user_show.tsx
+++ src/pages/user_show.tsx
@@ -1,79 +1,94 @@
-// user
-const user1 = () => {
+import Router from 'next/router'
 
 
-return (  
-<div >
+export default function user_show (){
+  console.log('こんにちは')
 
-<label class="relative block">
-  <span class="sr-only">Search</span>
-  <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-    <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><!-- ... --></svg>
-  </span>
-  <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
-</label>
+return(
+<>
+ <div className="bg-gray-400">    
+ <header className="container mx-auto text-white">
+   <div className="flex justify-between items-center">
+		<h1 className="text-4xl font-semibold"></h1>
+		<div>UserName</div>
+   </div>
+  </header>
+ </div>
 
-<div class="bg-gray-400">
-　<header class="container mx-auto text-white">
-     <div class="flex justify-between items-center">
-      <h1 class="text-4xl font-semibold"></h1>
-	  <div>
-       <h1>ユーザー名</h1>
-	  </div>
-	 </div>
-　</header>
-</div>
-
-
- <div class="grid grid-rows-3 grid-flow-col gap-4">
- <div class="row-span-3 border border-gray-600 ">
-	 <ul>
-       <li><a href="#" class="block px-8 py-2 hover:bg-gray-600">HOME</a></li>
-  　　　<li><a href="#" class="block px-8 py-2 hover:bg-gray-600">ユーザー管理</a></li>
+<div className="grid grid-rows-2 grid-flow-col ">
+  <div className="row-span-3 border border-gray-600 ">
+     <ul>
+       <li><a href="user" className="block px-8 py-2 hover:bg-gray-600">HOME</a></li>
+       <li><a href="user_show" className="block px-8 py-2 hover:bg-gray-600">ユーザー管理</a></li>
      </ul>
-		</div>
-		<div class="row-span-2 col-span-2 border border-gray-600 ...">
-			<h1>履歴</h1>
-			<table>
-			<thead>
-				<tr>
-				<th>日付</th>
-				<th>ユーザー</th>
-				<th>内容</th>
-			</tr>
-			</thead>
+  </div>
 
-			<tbody>
-			<tr>
-			<th>2022/3/４</th>
-			<th>user１</th>
-			<th>ユーザーを作成しました</th>
-			</tr>
-			<tr>
-			<th>2022/3/３</th>
-			<th>user２</th>
-			<th>ユーザーを作成しました</th>
-			</tr>
-			<tr>
-			<th>2022/3/2</th>
-			<th>user３</th>
-			<th>ユーザーを作成しました</th>
-			</tr>
-
-			</tbody>
-		</table>
-
-			</div>
-		</div>
-
-
-
-
-
+<div className="row-span-2 col-span-2 border border-gray-600 ...">
+ <span>検索</span>
+  <div className="flex flex-row ...">
+    <div className="text-gray-700 px-4 py-2  ">
+	 <span>id</span>	
+     <label for="text"></label>
+     <input type="text" name="id" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+    </div>
+    <div className="text-gray-700 px-4 py-2  ">
+     <span>名前</span>	
+	 <label for="text"></label>
+     <input type="text" name="name" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+    </div>
+    <div className="text-gray-700 px-4 py-2  ">
+     <span>メールアドレス</span>			
+      <label for="text"></label>
+      <input type="email" name="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+    </div>
+    <div className="text-gray-700 px-4 py-2  ">
+      <span>権限</span>	
+      <label for="text"></label>
+      <input type="text" name="class" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"  />
+    </div>
 </div>
- );
-};
+    <div className="text-gray-700 text-center px-4 py-2 ">
 
-export default user1;
+     <button className="bg-red-500 hover:bg-red-700  text-white font-bold py-2 px-4 rounded-full"
+       >検索
+     </button>
+     
+    </div>
 
+	<span>ユーザー一覧</span>
+    <table className="table-15">
+     <thead className="border-collapse border border-slate-400 ...">
+        <tr>
+         <th>id</th>
+         <th>名前</th>
+         <th>メールアドレス</th>
+		 <th>権限</th>
+        </tr>
+     </thead>
+     <tbody className="border-collapse border border-slate-400 ">
+        <tr>
+         <th>id_1</th>
+         <th>user1</th>
+         <th>user1@gmail.com</th>
+		 <th>管理者</th>
+        </tr>
+        <tr>
+         <th>id_2</th>
+         <th>user2</th>
+         <th>user2@gmail.com</th>
+		 <th>管理者</th>
+        </tr>
+        <tr>
+         <th>id_3</th>
+         <th>user3</th>
+         <th>user3@gmail.com</th>
+		 <th>ユーザー</th>
+        </tr>
+     </tbody>
+   </table>
+  </div>
+  </div>
 
+		</>
+)
+}
